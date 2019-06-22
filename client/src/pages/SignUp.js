c071ca77488d6c57595f5b472534f8ed04b6fc9e
Nicole <nalbrigh@kent.edu>
2019-06-22 12:27:28 -0400

    signup page element added

--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -4,7 +4,38 @@ import "./style.css";
 
 function SignUp() {
     return(
-        <div>This is the Sign Up Page</div>
+    
+ <div>
+
+        <form class="text-center border border-light p-5 signUpForm">
+
+            <p class="h4 mb-4">Sign up</p>
+
+            <div class="form-row mb-4">
+                <div class="col">
+
+                    <input type="text" id="defaultRegisterFormFirstName" class="form-control" placeholder="First name" />
+                </div>
+                <div class="col">
+
+                    <input type="text" id="defaultRegisterFormLastName" class="form-control" placeholder="Last name" />
+                </div>
+            </div>
+
+
+            <input type="email" id="defaultRegisterFormEmail" class="form-control mb-4" placeholder="E-mail" />
+
+            <input type="password" id="defaultRegisterFormPassword" class="form-control" placeholder="Password" aria-describedby="defaultRegisterFormPasswordHelpBlock" />
+            
+            <button class="btn btn-info my-4 btn-block" type="submit">Sign in</button>  
+            
+        </form>
+   </div>
+    
+
+
+            
+        
     );
 }
 
