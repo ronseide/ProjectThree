964a1308c983e1dcc840751f17f1da6d21be2178
Gabe <gungier1@gmail.com>
2019-06-27 21:17:03 -0400

    adjusted info sent back from user login

--- routes/Users.js
+++ routes/Users.js
@@ -88,10 +88,12 @@ users.post("/login", (req, res) => {
     .then(user => {
         if (user) {
             if(bcrypt.compareSync(req.body.password, user.password)) {
-                let token = jwt.sign(user.dataValues, process.env.SECRET_KEY, {
-                   expiresIn: 1440 
+                res.json({
+                    "user_id": user.id,
+                    "user_name": user.first_name,
+                    "last_name": user.last_name,
+                    "email": user.email
                 })
-                res.send(token)
             }
         } else {
             res.status(400).json({error: "User does not exist"})
