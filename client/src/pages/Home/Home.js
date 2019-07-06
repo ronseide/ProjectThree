15dc9f5d25ce7ecb51879464f1fe150d93b87db3
Nicole <nalbrigh@kent.edu>
2019-07-06 14:59:38 -0400

    style changes and updates to modal

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -6,10 +6,9 @@ import SignUp from "../SignUp/SignUp";
 
 function Home() {
   return (
-    <div class="container">
-      <div class="row">
-        <div class="col">
-          <h1>CryptoTracker</h1>
+  <div className="aboutContent">
+
+<h1>CryptoTracker</h1>
           <p>
             Cryptocurrency is amongst the most exciting financial developments to occur over the past decades. No longer tied to governments and precious metals, cryptocurrencies allow companies and entities to create whole new means of measuring value, conducting trade and building wealth. Millions of people around the world are today holding cryptocurrencies as a means of exchange and as an investment. <strong>Cryptocurrency investments can result in the resources needed for the finer things in life like yachts, mansions and luxury automobiles. </strong>
   
@@ -17,81 +16,22 @@ function Home() {
   
             <strong>Sign up to CryptoTracker today to step into the exciting world of cryptocurrency!</strong>
           </p>
-          <Link className="btn" to="" data-toggle="modal" data-target="#modalRegisterForm">
+          <Link className="secondSignUp btn" to="" data-toggle="modal" data-target="#modalRegisterForm">
             Sign Up
       </Link>&nbsp;
           <SignUp className="btn">
             Sign Up
       </SignUp>
-          <br></br>
-          <div class="divider">
-            <hr class="left" /><img src="./images/lineGraph1.png" width="25px" height="25px" alt="Line Graph" id="lineGraph"></img>
-            <hr class="right" />
-            <br></br>
-            <br></br>
 
-          </div>
-
-        </div>
-        <div class="col"><br></br><br></br>
+      <div>
 
 
+<img src="./images/tech_header.jpg" className="img-fluid z-depth-1 overlay" alt="Responsive image"/>
 
-          <div id="carousel-example-1z" class="carousel slide carousel-fade" data-ride="carousel">
+</div>
+</div>
 
-            <ol class="carousel-indicators">
-              <li data-target="#carousel-example-1z" data-slide-to="0" class="active"></li>
-              <li data-target="#carousel-example-1z" data-slide-to="1"></li>
-              <li data-target="#carousel-example-1z" data-slide-to="2"></li>
-            </ol>
-
-            <div class="carousel-inner" role="listbox">
-
-              <div class="carousel-item active">
-                <img class="d-block w-100 h-200" src="./images/yacht2.png" alt="First slide" />
-              </div>
-              {/* <div class="carousel-item active">
-                <img class="d-block w-100 h-200" src="https://mdbootstrap.com/img/Photos/Slides/img%20(141).jpg" alt="First slide" />
-              </div> */}
-
-
-              <div class="carousel-item">
-                <img class="d-block w-100 h-200"
-                  src="./images/mansion.png" alt="Second slide" />
-              </div>
-
-
-              {/* <div class="carousel-item">
-                <img class="d-block w-100 h-200" src="https://mdbootstrap.com/img/Photos/Slides/img%20(136).jpg" alt="Second slide" />
-              </div> */}
-
-              <div class="carousel-item">
-                <img class="d-block w-100 h-200" src="./images/rollsRoyce.png" alt="Third slide" />
-              </div>
-
-              {/* <div class="carousel-item">
-                <img class="d-block w-100 h-200" src="https://mdbootstrap.com/img/Photos/Slides/img%20(137).jpg" alt="Third slide" />
-              </div> */}
-
-            </div>
-
-            <a class="carousel-control-prev" href="#carousel-example-1z" role="button" data-slide="prev">
-              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-              <span class="sr-only">Previous</span>
-            </a>
-            <a class="carousel-control-next" href="#carousel-example-1z" role="button" data-slide="next">
-              <span class="carousel-control-next-icon" aria-hidden="true"></span>
-              <span class="sr-only">Next</span>
-            </a>
-
-          </div>
-          <br></br>
-          <br></br>
-          <br></br>
-        </div>
-
-      </div>
-    </div>
+    
 
 
 
