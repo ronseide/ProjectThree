c888e4557793c56521a28fd8bd0c20990ea1a171
Gabe <gungier1@gmail.com>
2019-07-11 16:08:24 -0400

    fixed database problems(please)

--- database/db.js
+++ database/db.js
@@ -1,7 +1,7 @@
 const Sequelize = require("sequelize")
 const db = {}
-const sequelize = new Sequelize("currency_tracker", "root", "password123", {
-    host: "/",
+const sequelize = new Sequelize("aipcxq54bp7y6799", "gn5oj9mm5pzz7z8l", "password123", {
+    host: "axxb6a0z2kydkco3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     dialect: "mysql",
     //    operatorsAliases: false,
 
