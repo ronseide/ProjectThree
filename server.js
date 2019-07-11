584dea2936928e567ac086415529c04e219f54ea
Gabe <gungier1@gmail.com>
2019-07-10 21:46:45 -0400

    fixed production server code

--- server.js
+++ server.js
@@ -2,11 +2,22 @@ var express = require("express")
 var cors = require("cors")
 var bodyParser = require("body-parser")
 var app = express()
+var path = require("path")
 var port = process.env.PORT || 3001
 // var db = require("./models")
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
+  app.use(express.static("client/build"));
+  app.get("/*", function(req, res) {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  });
+}
+
+else {
+  app.use(express.static(path.join(__dirname, '/client/public')));
+  app.get("/*", function(req, res) {
+    res.sendFile(path.join(__dirname, "./client/public/index.html"));
+  });
 }
 
 app.use(bodyParser.json())
